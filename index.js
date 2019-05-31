01f0730715f3511a7cf7418a3d934cf76ab29374
Song Zhang <yitengruntu12123@gmail.com>
2019-05-31 10:50:03 +0800

    basic finish

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 const { decode } = require('./decodeBase64.js')
+const { arrayFormat } = require('./arrayFormatOperator')
 
 decode('gfwlist.txt')
-  .then(fileName => {
-    console.log(fileName)
-  })
+  .then(fileName => arrayFormat(fileName))
 
 // console.log(decodeModule)
-
-// const readline = require('readline');
-// const path = require('path');
-// const fs = require('fs');
-// let filepath = path.join(__dirname, "gfwlist.txt")
-// let input = fs.createReadStream(filepath)
-// const rl = readline.createInterface({
-//   input: input
-// });
-// rl.on('line', (line) => {
-//   console.log(`Line from file: ${line}`);
-// });
-// rl.on('close', (line) => {
-//   console.log("读取完毕！");
-// });
